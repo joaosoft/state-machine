f197f0bea07a90b4654ea7e2b1dfbb64f784651b
João Ribeiro <joaosoft@gmail.com>
2018-08-02 21:19:55 +0100

    constants

--- consts.go
+++ consts.go
@@ -1,44 +1,27 @@
 package logger
 
-// Level ...
 type Level int
+type Prefix string
 
 const (
-	// DefaultLevel Level
-	DefaultLevel = InfoLevel
+	DefaultLevel = InfoLevel // DefaultLevel Level
 
-	// PanicLevel, when there is no recover
-	PanicLevel Level = iota
-	// FatalLevel, when the error is fatal to the application
-	FatalLevel
-	// ErrorLevel, when there is a controlled error
-	ErrorLevel
-	// WarnLevel, when there is a warning
-	WarnLevel
-	// InfoLevel, when it is a informational message
-	InfoLevel
-	// DebugLevel, when it is a debugging message
-	DebugLevel
+	PanicLevel Level = iota // PanicLevel, when there is no recover
+	FatalLevel              // FatalLevel, when the error is fatal to the application
+	ErrorLevel              // ErrorLevel, when there is a controlled error
+	WarnLevel               // WarnLevel, when there is a warning
+	InfoLevel               // InfoLevel, when it is a informational message
+	DebugLevel              // DebugLevel, when it is a debugging message
 
 	// Special Prefixes
-	// Add the level value to the prefix
-	LEVEL = "{{LEVEL}}"
-	// Add the timestamp value to the prefix
-	TIMESTAMP = "{{TIMESTAMP}}"
-	// Add the date value to the prefix
-	DATE = "{{DATE}}"
-	// Add the time value to the prefix
-	TIME = "{{TIME}}"
-	// Add the client ip address
-	IP = "{{IP}}"
-	// Add the error trace
-	TRACE = "{{TRACE}}"
-	// Add the package name
-	PACKAGE = "{{PACKAGE}}"
-	// Add the file
-	FILE = "{{FILE}}"
-	// Add the function name
-	FUNCTION = "{{FUNCTION}}"
-	// Add the debug stack
-	STACK = "{{STACK}}"
+	LEVEL     Prefix = "{{LEVEL}}"     // Add the level value to the prefix
+	TIMESTAMP Prefix = "{{TIMESTAMP}}" // Add the timestamp value to the prefix
+	DATE      Prefix = "{{DATE}}"      // Add the date value to the prefix
+	TIME      Prefix = "{{TIME}}"      // Add the time value to the prefix
+	IP        Prefix = "{{IP}}"        // Add the client ip address
+	TRACE     Prefix = "{{TRACE}}"     // Add the error trace
+	PACKAGE   Prefix = "{{PACKAGE}}"   // Add the package name
+	FILE      Prefix = "{{FILE}}"      // Add the file
+	FUNCTION  Prefix = "{{FUNCTION}}"  // Add the function name
+	STACK     Prefix = "{{STACK}}"     // Add the debug stack
 )
